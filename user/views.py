4653f0f30353867f5862490fe8ff63b3a8460c9d
김예진 <jin11241124@gmail.com>
2020-10-22 21:03:51 +0900

    회원가입, 로그인 구현

--- user/views.py
+++ user/views.py
@@ -1,9 +1,12 @@
 import json
 import re
+import bcrypt
+import jwt
 
 from django.http import JsonResponse
 from django.views import View
 from user.models import User
+from my_settings import SECRET_KEY,ALGORITHM
 
 
 
@@ -12,28 +15,46 @@ class SignUpView(View):
     def post(self,request):
         data = json.loads(request.body)
 
-        email_compile = r"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?"
+        email_test      = '^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-_]+\.[a-zA-Z.]{2,3}$'
+        password_test   = '^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$'
 
         try :
-            if '@' not in data['email'] and '.' not in data['email'] :
-                return JsonResponse({'MESSAGE' : 'EMAIL_ERROR'}, status = 400)
+            if re.match(email_test, data['email']) == None :
+                return JsonResponse({'MESSAGE' : 'EMAIL_ERORR'}, status = 400)
 
-            elif len(data['password'] < 6 : 
-                     return JsonResponse({'MESSAGE' : 'SHORT_PASSWORD'}, status = 400)
+            elif re.match(password_test, data['password']) == None : 
+                return JsonResponse({'MESSAGE' : 'PASSWORD_ERROR'}, status = 400)
 
-            elif Users.objects.filter(email = data['email']).exists():
-                     return JsonResponse({'MESSAGE' : 'EMAIL_OVERLAP'}, status = 400)
+            elif User.objects.filter(email = data['email']).exists():
+                return JsonResponse({'MESSAGE' : 'EMAIL_OVERLAP'}, status = 400)
 
             else :
+                    User.objects.create(
+                        name = data['name'], 
+                        email = data['email'],
+                        password = bcrypt.hashpw(data['password'].encode('utf-8'), bcrypt.gensalt()).decode())
+                    return JsonResponse({'MESSAGE' : 'SUCCESS'}, status = 200)
 
-            name = data['name']
-            email = data['email']
-            password = data['password']
+        except KeyError:
+            return JsonResponse({'MESSAGE' : 'KEY_ERROR'}, status = 400)
 
-            User.objects.create(
-                name= name, 
-                email= email, 
-                password= password,
-            )
-        
 
+
+
+class LoginView(View):
+
+    def post(self,request):
+        data = json.loads(request.body)
+
+        try :
+            if User.objects.filter(email=data['email']).exists():
+                db_email= User.objects.get(email=data['email'])
+                if bcrypt.checkpw(data['password'].encode('utf-8'),db_email.password.encode('utf-8')) == True:
+                    return JsonResponse({'MESSAGE' : 'SUCCESS', 'AUTHORIZATION' : jwt.encode({'id' : db_email.id}, SECRET_KEY, ALGORITHM).decode()}, status=200)
+                else : 
+                    return JsonResponse({'MESSAGE' : 'EMAIL_OR_PASSWORD_ERROR'}, status=400)
+            else :
+                return JsonResponse({'MESSAGE' : 'EMAIL_DOES_NOT_EXIST'}, status=400)
+
+        except KeyError:
+            return JsonResponse({'MESSAGE' : 'KEY_ERROR'}, status=400)
