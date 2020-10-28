59de71675130b80faf01f8df631f9b160ba543e4
adriankim <adriankim@Adrians-MacBook-Pro.local>
2020-10-28 14:44:53 +0900

    edited views

--- movie/urls.py
+++ movie/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from movie.views import MovieView, ActorView, FrontView, MoviesView
+from movie.views import MovieView, ActorView, FrontView, MoviesView, RelatedMovieView
 
 urlpatterns = [
 	path('/movies', MoviesView.as_view()),
 	path('/movie/<int:movie_id>', MovieView.as_view()),
 	path('/actor/<int:person_id>', ActorView.as_view()),
-	path('/front', FrontView.as_view())
+	path('/front', FrontView.as_view()),
+	path('/movies/<int:movie_id>/related', RelatedMovieView.as_view())
 ]
--- movie/views.py
+++ movie/views.py
@@ -18,37 +18,20 @@ class FrontView(View):
 			'country': movie.country,
 			'poster' : movie.poster_url,
 			'runtime': movie.run_time,
-			'service': list(movie.service.values_list('name',flat= True)),
+			'service': list(movie.service.values()),
 			'genre'	 : list(movie.genre.values_list('name', flat= True))
 			} for movie in filtered_list[:20]]
 			return info
 		library = Movies.objects.all()
-#IN THEATERS
-		library_listed 		= [titles for titles in library]
-		theater_filtered 	= [title for title in library if title.in_theaters== 1]
-#WATCHA ROW
-		watcha_filtered 	= library.filter(service = '2')
-#NETFLIX ROW
-		netflix_filtered	= library.filter(service = '1')
-#KOREA-BEST ROW
-		library_listed 		= [titles for titles in library]
-		korea_filtered 		= [title for title in library if title.country == '한국']
-#US-BEST ROW
-		library_listed 		= [titles for titles in library]
-		us_filtered 	   	= [title for title in library if title.country == '미국']
-#ACTION ROW
-		action_filtered	= library.filter(genre = '2')
-#COMEDY ROW
-		comedy_filtered 	= library.filter(genre = '12')
 
 		return JsonResponse ({
-							'theater'	: info_lister(theater_filtered),
-							'watcha' 	: info_lister(watcha_filtered),
-							'netflix'	: info_lister(netflix_filtered), 
-							'kr'		: info_lister(korea_filtered),
-							'us'		: info_lister(us_filtered),
-							'action'	: info_lister(action_filtered),
-							'comedy'	: info_lister(comedy_filtered),
+							'theater'	: info_lister([title for title in library if title.in_theaters== 1]),
+							'watcha' 	: info_lister(library.filter(service = '2')),
+							'netflix'	: info_lister(library.filter(service = '1')), 
+							'kr'		: info_lister([title for title in library if title.country == '한국']),
+							'us'		: info_lister([title for title in library if title.country == '미국']),
+							'action'	: info_lister(library.filter(genre = '2')),
+							'comedy'	: info_lister(library.filter(genre = '12')),
 							}, status=200)
 
 class MoviesView(View):
@@ -65,6 +48,18 @@ class MoviesView(View):
 					} for movie in Movies.objects.all()]
 		return JsonResponse ({'movielist' : info }, status=200)
 
+class RelatedMovieView(View):
+	def get(self, request, movie_id) :
+		genre_comparer	= Movies.objects.get(id = movie_id)
+		movie_library   = Movies.objects.all()
+		related_id 		= [moviebygenre.id for moviebygenre in movie_library if not set(genre_comparer.genre.values_list('name', flat=True)).isdisjoint(moviebygenre.genre.values_list('name', flat=True))]
+		related_objects = [Movies.objects.get(id = ids) for ids in related_id]
+		info 			= [{'movieposter_url': movie.poster_url, 
+							'title' 		 : movie.title,
+							'country'		 : movie.country,
+							'genre'			 : list(movie.genre.values_list('name', flat=True))} 
+							for movie in related_objects]
+		return JsonResponse ({'relate_movies': info }, status=200)
 
 class MovieView(View) :
 	def get(self, request, movie_id) :
