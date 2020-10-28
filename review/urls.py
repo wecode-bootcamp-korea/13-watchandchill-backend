f27e2647e2b91ad4325b1d73f20e1898c78cac3c
adriankim <adriankim@Adrians-MacBook-Pro.local>
2020-10-28 20:06:20 +0900

    status progress save

--- review/urls.py
+++ review/urls.py
@@ -1,6 +1,6 @@
 from django.urls    import path
-from .views         import MoviePageRatingView
+#from .views         import MoviePageRatingView
 
 urlpatterns = [
-    path('/<int:movie_id>',MoviePageRatingView.as_view())
+#     path('/<int:movie_id>',MoviePageRatingView.as_view())
 ]
--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import SignUpView, LoginView
+# StatusSelectorView
 
 urlpatterns = [
     path('',SignUpView.as_view()),
-    path('/login',LoginView.as_view())
+    path('/login',LoginView.as_view()),
+    # path('/status/<int:status_id>/movie/<int:movie_id>', StatusSelectorView.as_view())
 ]
