a322f026034eb771f98848e007ccb05a74ad9ffb
김예진 <jin11241124@gmail.com>
2020-10-29 20:18:43 +0900

    삭제: 리뷰앱 삭제

--- watchapedia/settings.py
+++ watchapedia/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     'corsheaders',
     'movie',
     'user',
-    'review',
 ]
 
 MIDDLEWARE = [
--- watchapedia/urls.py
+++ watchapedia/urls.py
@@ -3,5 +3,4 @@ from django.urls    import path, include
 urlpatterns = [
     path('user', include('user.urls')),
 	path('movie', include('movie.urls')),
-    path('review', include('review.urls'))
 ]
