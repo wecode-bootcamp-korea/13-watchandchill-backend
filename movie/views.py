23376e6aa2e58690f20530a8ac87afd6f87f1074
김예진 <jin11241124@gmail.com>
2020-10-29 20:00:11 +0900

    checkout commit

--- movie/views.py
+++ movie/views.py
@@ -6,8 +6,6 @@ from django.http import JsonResponse
 from django.views import View
 #Custom
 from movie.models import Movies, MoviePhotos, MovieVideos, Cast, People, Genres, MovieGenres, Tags, MovieTags, Services, MovieServices
-
-
 class FrontView(View):
 	def get(self, request) :
 		def info_lister(filtered_list):
@@ -23,39 +21,6 @@ class FrontView(View):
 			} for movie in filtered_list[:20]]
 			return info
 		library = Movies.objects.all()
-
-#IN THEATERS
-		library_listed 		= [titles for titles in library]
-		theater_filtered 	= [title for title in library if title.in_theaters== 1]
-#WATCHA ROW
-		watcha_filtered 	= library.filter(service = '2')
-#NETFLIX ROW
-		netflix_filtered	= library.filter(service = '1')
-#KOREA-BEST ROW
-		library_listed 		= [titles for titles in library]
-		korea_filtered 		= [title for title in library if title.country == '한국']
-#US-BEST ROW
-		library_listed 		= [titles for titles in library]
-		us_filtered 	   	= [title for title in library if title.country == '미국']
-#ACTION ROW
-		action_filtered	= library.filter(genre = '2')
-#COMEDY ROW
-		comedy_filtered 	= library.filter(genre = '12')
-		return JsonResponse ({
-							'theater'	: info_lister(theater_filtered),
-							'watcha' 	: info_lister(watcha_filtered),
-							'netflix'	: info_lister(netflix_filtered), 
-							'kr'		: info_lister(korea_filtered),
-							'us'		: info_lister(us_filtered),
-							'action'	: info_lister(action_filtered),
-							'comedy'	: info_lister(comedy_filtered),
-							}, status=200)
-
-class MovieView(View) :
-	def get(self, request, movie_id) :
-		try:
-			data 	 	= json.loads(request.body)
-
 		return JsonResponse ({
 							'theater'	: info_lister([title for title in library if title.in_theaters== 1]),
 							'watcha' 	: info_lister(library.filter(service = '2')),
@@ -65,7 +30,6 @@ class MovieView(View) :
 							'action'	: info_lister(library.filter(genre = '2')),
 							'comedy'	: info_lister(library.filter(genre = '12')),
 							}, status=200)
-
 class MoviesView(View):
 	def get(self, request) :
 		info = [{
@@ -79,7 +43,6 @@ class MoviesView(View):
 		'genre'	 : list(movie.genre.values_list('name', flat= True))
 					} for movie in Movies.objects.all()]
 		return JsonResponse ({'movielist' : info }, status=200)
-
 class RelatedMovieView(View):
 	def get(self, request, movie_id) :
 		genre_comparer	= Movies.objects.get(id = movie_id)
@@ -92,7 +55,6 @@ class RelatedMovieView(View):
 							'genre'			 : list(movie.genre.values_list('name', flat=True))} 
 							for movie in related_objects]
 		return JsonResponse ({'relate_movies': info }, status=200)
-
 class MovieView(View) :
 	def get(self, request, movie_id) :
 		try:
@@ -111,24 +73,18 @@ class MovieView(View) :
 				'runtime'		: movie.run_time,
 				'coverpic_url'	: movie.coverpic_url,
 				'description'	: movie.description,
+				'star_review'	: '4',
+				'status'		: '보고싶어요',
 				'cast'			: [{
 									'name': person.name, 
 									'role': Cast.objects.filter(movie = movie , name = person)[0].role, 
 									'cast_as': Cast.objects.filter(movie = movie , name = person)[0].cast_as, 
 									'photo': person.avatar_url
-									} 
-				for person in movie.casting.all()]
-			}
-			return JsonResponse ({'movie_information': all_info}, status=200)
-		except Movies.DoesNotExist:
-			return JsonResponse ({'KeyError': 'Non-existant movie id'}, status = 404)
 									} for person in movie.casting.all()]
 			}
-
 			return JsonResponse ({'movie_information': all_info}, status=200)
 		except Movies.DoesNotExist:
 			return JsonResponse ({'KeyError': 'Non-existant movie id'}, status = 404)
-
 class ActorView(View) :
 	def get(self, request, person_id) :
 		try:
